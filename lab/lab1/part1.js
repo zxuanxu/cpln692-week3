b4e43e3ef725bf6b6ef21bf85be74a1795fb8649
ross <rossbernet@gmail.com>
2019-01-29 08:26:23 -0500

    added back last years

--- lab/lab1/part1.js
+++ lab/lab1/part1.js
@@ -7,47 +7,11 @@ Instructions: "Write a function that adds one to the number provided"
 Example: "plusOne(2) should return 3"
 ===================== */
 
-var plusOne = function(num) {
-  return num + 1
+var plusOne = function() {
+  /* Your code here */
 };
 
 console.log('plusOne success:', plusOne(99) === 100);
-
-
-/* =====================
-Instructions: "Write a function that adds two to the number provided"
-Example: "plusTwo(2) should return 3"
-NOTE: Use the `plusOne` function in the body of your `plusTwo` function
-===================== */
-
-var plusTwo = function(num) {
-  return plusOne(num) + 1
-};
-
-console.log('plusTwo success:', plusTwo(99) === 101);
-
-
-
-/* =====================
-Instructions: "Write a function that checks to see if a number is an integer,
-if so, it returns even or odd depending on the number, otherwise it returns "error"
-
-===================== */
-
-var oddOrEven = function(num) {
-  if (num % 2 === 0) {
-    return 'even'
-  } else if (num % 1 === 0) {
-    return 'odd'
-  } else {
-    return 'error'
-  }
-};
-
-console.log('oddOrEven success:', oddOrEven(100) === 'even' && oddOrEven(201) === 'odd');
-
-
-
 
 /* =====================
 Instructions: "Write a function, age, that takes a birth year and returns an age in years."
@@ -55,59 +19,34 @@ Instructions: "Write a function, age, that takes a birth year and returns an age
 Example: "age(2000) should return 17"
 ===================== */
 
-var age = function(birth) {
-  return 2019 - birth
-};
+var age = function(birth) {};
 
-console.log('age success:', age(1971) === 48);
+console.log('age success:', age(1971) === 46);
 
 /* =====================
 Instructions: "Write a function that returns true for numbers over 9000 and false otherwise"
 Example: "over9000(22) should return false"
 ===================== */
 
-var over9000 = function(num) {
-  if (num >9000) {
-    return true
-  } else {
-    return false
-  }
-};
+var over9000 = function() {};
 
 console.log('over9000 success:', over9000(9001) === true && over9000(12) === false);
 
-
-
 /* =====================
-Instructions: "Write a function, trump, that checks to see if the input is a string
-and if it is, returns the text in all upper case
-and if it is not, it prints to the console, "Please use a string with this function""
+Instructions: "Write a function that returns the value of an object at a specified key"
+Example: "valueAtKey({'name': 'Nathan'}, 'name') should return 'Nathan'"
 ===================== */
 
+var valueAtKey = function(){};
 
-var trump = function(tweet) {
-  if (typeof(tweet) !== 'string') {
-    return "Please use a string with this function"
-  } else {
-    return tweet.toUpperCase()
-  }
-
-};
-
-console.log('trump success:', trump(12) === "Please use a string with this function" && trump('hi') === 'HI');
-
-
+console.log('valueAtKey success:', valueAtKey({'foo': 'bar'}, 'foo') === 'bar');
 
 /* =====================
 Instructions: "Write a function which returns the y coordinate of a line given m, x, and b"
 Example: "y(0, 0, 0) should return 0; y(1, 1, 1) should return 2"
 ===================== */
 
-
-var y = function(m,x,b) {
-    return (m*x)+b
-
-};
+var y = function() {};
 
 console.log('y success:', y(12, 1, 12) === 24);
 
